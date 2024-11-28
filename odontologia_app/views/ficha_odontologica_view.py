bd1208d10e5f0b02df37addd145f3383e7d62b98
ThePepinaco <thepepinaco@gmail.com>
2024-11-28 14:23:44 -0500

    ficha_odntologica_view responsive

--- odontologia_app/views/ficha_odontologica_view.py
+++ odontologia_app/views/ficha_odontologica_view.py
@@ -12,8 +12,16 @@ def view_fichas_odontologicas(self, cliente_id):
     cliente = ClienteController.obtener_cliente_por_id(cliente_id)
     fichas_window.title(f"Fichas odontológicas de {cliente.nombre}")
     fichas_window.geometry("900x600")
+    fichas_window.minsize(820,500)
     fichas_window.attributes('-topmost', True)
     fichas_window.after(100, lambda: fichas_window.attributes('-topmost', False))
+
+        
+    self.large_font = ("Arial", 18, "bold")
+    self.medium_font = ("Arial", 16)
+    self.small_font = ("Arial", 12)
+    
+    
     def calcular_saldo_total():
         saldo_total=0
         sum_costo=0
@@ -33,14 +41,24 @@ def view_fichas_odontologicas(self, cliente_id):
     saldo_total=0
     # Etiqueta para el título
     saldo_total_var = customtkinter.StringVar(value=f"Saldo total: {saldo_total:.2f}")
-    label = customtkinter.CTkLabel(fichas_window, text=f"Fichas Odontológicas de: {cliente.nombre}", font=("Arial", 16))
+    label = customtkinter.CTkLabel(fichas_window, text=f"Fichas Odontológicas de: {cliente.nombre}", font=self.large_font)
     label.pack(pady=10)
     saldo_label = customtkinter.CTkLabel(fichas_window, textvariable=saldo_total_var, font=("Arial", 16))
     saldo_label.pack(pady=10)
+    
+    
+    container_ficha_odo = customtkinter.CTkFrame(fichas_window)
+    container_ficha_odo.pack(fill="both", expand=True, padx=10, pady=10)
+    
+    # Configuración del grid en el contenedor
+    container_ficha_odo.grid_rowconfigure(0, weight=1)  # Frame desplazable ocupa espacio ajustable
+    container_ficha_odo.grid_rowconfigure(1, weight=0)  # Espacio fijo para el botón
+    container_ficha_odo.grid_columnconfigure(0, weight=1)  # Ajusta el ancho dinámicamente
     calcular_saldo_total()
     # Frame para contener la tabla y el scrollbar
-    frame = customtkinter.CTkFrame(fichas_window)
-    frame.pack(fill="both", expand=True, padx=10, pady=10)
+    frame = customtkinter.CTkFrame(container_ficha_odo)
+    frame.grid(row=0, column=0, sticky="nsew", padx=10, pady=10)
+    #frame.pack(fill="both", expand=True, padx=10, pady=10)
 
     # Tabla para mostrar las fichas
     tree_ficha_odonto = ttk.Treeview(
@@ -54,11 +72,11 @@ def view_fichas_odontologicas(self, cliente_id):
     tree_ficha_odonto.heading("costo", text="Costo")
     tree_ficha_odonto.heading("abono", text="Abono")
     tree_ficha_odonto.heading("saldo", text="Saldo")
-    tree_ficha_odonto.column("fecha", anchor="center", width=100)
+    tree_ficha_odonto.column("fecha", anchor="center", width=115, stretch="no")
     tree_ficha_odonto.column("tratamiento", anchor="w", width=200)
-    tree_ficha_odonto.column("costo", anchor="center", width=80)
-    tree_ficha_odonto.column("abono", anchor="center", width=80)
-    tree_ficha_odonto.column("saldo", anchor="center", width=80)
+    tree_ficha_odonto.column("costo", anchor="center", width=110, stretch="no")
+    tree_ficha_odonto.column("abono", anchor="center", width=110, stretch="no")
+    tree_ficha_odonto.column("saldo", anchor="center", width=110, stretch="no")
     tree_ficha_odonto.pack(side="left", fill="both", expand=True)
     
     # Scrollbar vertical usando customtkinter
@@ -121,8 +139,8 @@ def view_fichas_odontologicas(self, cliente_id):
             # Crear ventana para editar la ficha
             editar_window = customtkinter.CTkToplevel(fichas_window)
             editar_window.title("Editar Ficha Odontológica")
-            editar_window.geometry("400x500")
-
+            editar_window.geometry("300x500")
+            editar_window.minsize(300,500)
             # Crear campos para editar
             labels = ["Fecha", "Tratamiento", "Costo", "Abono", "Saldo"]
             values = [
@@ -135,21 +153,21 @@ def view_fichas_odontologicas(self, cliente_id):
             entries = []
 
             for i, label_text in enumerate(labels):
-                label = customtkinter.CTkLabel(editar_window, text=label_text)
+                label = customtkinter.CTkLabel(editar_window, text=label_text, font=self.medium_font)
                 label.pack(pady=5)
                 if i == 0:  # Campo de fecha con calendario
-                    date_entry = DateEntry(editar_window, date_pattern="yyyy-mm-dd")
+                    date_entry = DateEntry(editar_window, date_pattern="yyyy-mm-dd", font=self.small_font)
                     date_entry.pack(pady=5)
                     date_entry.set_date(values[i])
                     entries.append(date_entry)
                 elif i==4:
-                    entry = customtkinter.CTkEntry(editar_window, textvariable=saldo_var_fichas, state="disabled")
+                    entry = customtkinter.CTkEntry(editar_window, textvariable=saldo_var_fichas, state="disabled", font=self.medium_font)
                     entry.pack(pady=5)
                     entry.insert(0, str(values[i]))
                     entries.append(entry)
                     
                 else:
-                    entry = customtkinter.CTkEntry(editar_window)
+                    entry = customtkinter.CTkEntry(editar_window, font=self.medium_font)
                     entry.pack(pady=5)
                     entry.insert(0, str(values[i]))
                     entries.append(entry)
@@ -181,10 +199,10 @@ def view_fichas_odontologicas(self, cliente_id):
                 calcular_saldo_total()
                 editar_window.destroy()
 
-            guardar_button = customtkinter.CTkButton(editar_window, text="Guardar", command=guardar_cambios)
+            guardar_button = customtkinter.CTkButton(editar_window, text="Guardar", command=guardar_cambios, font=self.medium_font)
             guardar_button.pack(pady=10)
 
-            cancelar_button = customtkinter.CTkButton(editar_window, text="Cancelar", command=editar_window.destroy)
+            cancelar_button = customtkinter.CTkButton(editar_window, text="Cancelar", command=editar_window.destroy, font=self.medium_font)
             cancelar_button.pack(pady=10)
 
     # Función para agregar una nueva ficha
@@ -228,36 +246,36 @@ def view_fichas_odontologicas(self, cliente_id):
         except ValueError:
             saldo_var_fichas.set("0.00")
     # Crear botones para eliminar y editar fichas
-    botones_frame = customtkinter.CTkFrame(fichas_window)
-    botones_frame.pack(pady=10)
+    botones_frame = customtkinter.CTkFrame(container_ficha_odo)
+    botones_frame.grid(row=1, column=0, sticky="", padx=10, pady=10)
 
-    eliminar_button = customtkinter.CTkButton(botones_frame, text="Eliminar", command=eliminar_ficha, fg_color="#F24236")
+    eliminar_button = customtkinter.CTkButton(botones_frame, text="Eliminar", command=eliminar_ficha, fg_color="#F24236", font=self.medium_font)
     eliminar_button.grid(row=0, column=0, padx=10)
 
-    editar_button = customtkinter.CTkButton(botones_frame, text="Editar", command=editar_ficha)
+    editar_button = customtkinter.CTkButton(botones_frame, text="Editar", command=editar_ficha, font=self.medium_font)
     editar_button.grid(row=0, column=1, padx=10)
     # Crear campos para agregar nueva ficha
-    form_frame_odntologia = customtkinter.CTkFrame(fichas_window)
-    form_frame_odntologia.pack(pady=10)
+    form_frame_odntologia = customtkinter.CTkFrame(container_ficha_odo)
+    form_frame_odntologia.grid(row=2, column=0, sticky="", padx=10, pady=10)
 
     labels = ["Fecha", "Tratamiento", "Costo", "Abono", "Saldo"]
     entries = []
     saldo_var_fichas = customtkinter.StringVar(value="0.00")
     for i, label_text in enumerate(labels):
-        label = customtkinter.CTkLabel(form_frame_odntologia, text=label_text)
+        label = customtkinter.CTkLabel(form_frame_odntologia, text=label_text, font=self.medium_font)
         label.grid(row=0, column=i, padx=5)
         if i == 0:  # Campo de fecha con calendario
-            date_entry = DateEntry(form_frame_odntologia, date_pattern="yyyy-mm-dd")
+            date_entry = DateEntry(form_frame_odntologia, date_pattern="yyyy-mm-dd", font=self.small_font)
             date_entry.grid(row=1, column=i, padx=5)
             date_entry.set_date(datetime.now())
             entries.append(date_entry)
         elif i==4:
-            entry = customtkinter.CTkEntry(form_frame_odntologia, textvariable=saldo_var_fichas, state="disabled")
+            entry = customtkinter.CTkEntry(form_frame_odntologia, textvariable=saldo_var_fichas, state="disabled", font=self.medium_font)
             entry.grid(row=1, column=i, padx=5)
             entries.append(entry)
             
         else:
-            entry = customtkinter.CTkEntry(form_frame_odntologia)
+            entry = customtkinter.CTkEntry(form_frame_odntologia, font=self.medium_font)
             entry.grid(row=1, column=i, padx=5)
             entries.append(entry)
     costo_entry = entries[2]
@@ -266,8 +284,8 @@ def view_fichas_odontologicas(self, cliente_id):
     costo_entry.bind("<KeyRelease>", lambda event: calcular_saldo())
     abono_entry.bind("<KeyRelease>", lambda event: calcular_saldo())
     # Botón para agregar ficha
-    add_button = customtkinter.CTkButton(fichas_window, text="Agregar Ficha", command=agregar_ficha)
-    add_button.pack(pady=10)
+    add_button = customtkinter.CTkButton(container_ficha_odo, text="Agregar Ficha", command=agregar_ficha, font=self.medium_font)
+    add_button.grid(row=3, column=0, sticky="", padx=10, pady=10)
     # Botón para cerrar
-    close_button = customtkinter.CTkButton(fichas_window, text="Cerrar", command=fichas_window.destroy)
-    close_button.pack(pady=10)
+    close_button = customtkinter.CTkButton(container_ficha_odo, text="Cerrar", command=fichas_window.destroy, font=self.medium_font)
+    close_button.grid(row=4, column=0, sticky="", padx=10, pady=10)
